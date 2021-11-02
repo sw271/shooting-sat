41cac18e7c5057630353c6138557a4925fdbffc8
Simon Warren <admin@simon-warren.com>
2021-11-02 22:48:56 +0000

    adding api to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,68 @@
 import "./App.css";
 import { generateMockData } from "./mock/mockdata";
 import { SatellitePassTable } from "./components/SatellitePassTable";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { ISatellitePass } from "./interfaces/ISatellitePass";
+import { SatellitePass } from "./interfaces/SatelitePass";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000",
+  cache: new InMemoryCache(),
+});
+
+const LAT = 51.454514;
+const LNG = -2.58791;
+
+const GET_PASSES = gql`
+  query GetPasses($lat: Float!, $lng: Float!) {
+    getPasses(input: { lat: $lat, lng: $lng }) {
+      passes {
+        name
+        riseDatetime
+        riseAzimuth
+        setDatetime
+        setAzimuth
+        isVisible
+      }
+    }
+  }
+`;
 
 const mockData = generateMockData();
 
 function App() {
+  const { loading, error, data } = useQuery<{
+    getPasses: { passes: ISatellitePass[] };
+  }>(GET_PASSES, {
+    variables: {
+      lat: LAT,
+      lng: LNG,
+    },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error || !data) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
     <div className="App">
-      <SatellitePassTable data={mockData} />
+      <SatellitePassTable
+        data={data.getPasses.passes.map((x) => new SatellitePass(x))}
+      />
     </div>
   );
 }
 
-export default App;
+const AppProvider: React.FC = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+export default AppProvider;
--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from ariadne.asgi import GraphQL
+from starlette.middleware.cors import CORSMiddleware
 from api.schema import schema
 
-
-app = GraphQL(schema, debug=True)
+app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=[
+                     '*'], allow_methods=("GET", "POST", "OPTIONS"))
