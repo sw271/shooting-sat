984ec509f3fc919c1e4c44f4f04a0e1ec8851d0b
simon <nonlinear.spikes@gmail.com>
2021-12-14 23:17:41 +0000

    catching error while restting store!

--- client/src/screens/SatellitesScreen.tsx
+++ client/src/screens/SatellitesScreen.tsx
@@ -1,6 +1,7 @@
 import {
   useQuery,
   gql,
+  useApolloClient,
 } from "@apollo/client";
 import {
   EEventType,
@@ -16,6 +17,7 @@ import { AppBar, Box, Button, ButtonGroup, Container, Toolbar } from "@mui/mater
 import { ZOOM_WHEN_LOCATED } from "./LocationScreen";
 import { Location } from "../models/Location"
 import { mutations } from "../operations/mutations";
+import { InvariantError } from "@apollo/client/utilities/globals";
 
 const GET_EVENTS = gql`
   query GetEvents($lat: Float!, $lng: Float!, $dateFromIncUtc: String) {
@@ -52,7 +54,17 @@ const App: React.FC<Props2> = (props) => {
             lng: props.location.longitude,
             dateFromIncUtc: data.getEvents.dateToExcUtc,
           },
-        });
+        })
+          .catch(e => {
+            if (e instanceof InvariantError) {
+              if (e.message === "Store reset while query was in flight (not completed in link chain)") {
+                // pass: this is ok, we have probably hit the "forgert me button"
+                console.log("Error caught while restting store");
+                return;
+              }
+            }
+            throw e;
+          });
       }
     }
   });
@@ -67,7 +79,6 @@ const App: React.FC<Props2> = (props) => {
   if (loading) return <p>Loading...</p>;
   if (error || !data) return <p>Error :(</p>;
 
-  console.log(data);
   const passes: ISatellitePass[] = [];
   data.getEvents.satelliteEvents.forEach((sat) => {
     let riseDatetime: Date | undefined = undefined;
@@ -109,11 +120,16 @@ interface Props {
 }
 
 export const SatellitesScreen: React.FC<Props> = (props) => {
-
+  const client = useApolloClient();
   return (
     <div>
       <ButtonGroup fullWidth>
-        <Button>Forget Me</Button>
+        <Button
+          onClick={() => {
+            mutations.setLocation(undefined);
+            client.resetStore();
+          }}
+        >Forget Me</Button>
         <Button
           onClick={() => mutations.setShowLocationScreen(true)}
         >Change Location</Button>
